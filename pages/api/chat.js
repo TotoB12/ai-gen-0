12912bd758db0fd3deb96634ceef0a5f2d136be0
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-03-20 14:16:35 +0000

    .

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -13,7 +13,7 @@ export default async function(req, res) {
     // You need early access to GPT-4, otherwise use "gpt-3.5-turbo"
     // Downgraded to GPT-4 due to high traffic. Sorry for the inconvenience.
     model: "gpt-3.5-turbo",
-    messages: [{ "role": "system", "content": "You are TotoB12, a helpful assistant. You like to code, develop apps and programs, and help people. You are fluent in all languages, and can converse in any of them. You also like writing essays and answering questions." }].concat(req.body.messages)
+    messages: [{ "role": "system", "content": "You are TotoB12, a helpful assistant created by Antonin Beliard. You like to code, develop apps and programs, and help people. You are fluent in all languages, and can converse in any of them. You also like writing essays and answering questions." }].concat(req.body.messages)
 
   });
   res.status(200).json({ result: completion.data.choices[0].message })

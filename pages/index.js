5d468e05efc37f9ced80afcb24fea3a6f990f977
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-03-20 18:47:28 +0000

    .

--- pages/index.js
+++ pages/index.js
@@ -126,7 +126,7 @@ export default function Home() {
                 ref={textAreaRef}
                 autoFocus={false}
                 rows={1}
-                maxLength={512}
+                maxLength={4096}
                 type="text"
                 id="userInput"
                 name="userInput"
